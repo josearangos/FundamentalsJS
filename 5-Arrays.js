1c6bc0dc14b9d779542bb502bd1ad3f8d294a0e9
josearangos <jose.arangos@udea.edu.co>
2019-06-15 21:46:37 -0500

    review arrays

--- 5-Arrays.js
+++ 5-Arrays.js
@@ -32,7 +32,7 @@ var vicky = {
   altura: 1.56,
   cantidadDeLibros: 91
 };
-
+console.log("Convert TO CMS 2: ", PersonsWithTallInCms2);
 var paula = {
   nombre: "Paula",
   apellido: "Barros",
@@ -54,19 +54,26 @@ for (var i = 0; i < Persons.length; i++) {
 
 const isTall = ({ altura }) => altura > 1.8;
 var tallPeoples = Persons.filter(isTall);
-console.log(tallPeoples);
+console.log("Peoples FILTER  1 OPTION", tallPeoples);
 
 //2 option
-
 var tallPeoples2 = Persons.filter(function(Person) {
   return Person.altura > 1.8;
 });
 
-console.log(tallPeoples2);
+console.log("Peoples, FILTER 2 OPTION:", tallPeoples2);
 
+const tallBetween = ({ altura }) => (altura > 1.5) & (altura < 1.73);
+var tallBetweenPersons1 = Persons.filter(tallBetween);
+
+const tallBetweenPersons2 = Persons.filter(function(person) {
+  return (person.altura > 1.5) & (person.altura <= 1.71);
+});
+
+console.log(" 1)Personas with Tall between 1.5 and 1.7 ", tallBetweenPersons1);
+console.log(" 2)Personas with Tall between 1.5 and 1.7 ", tallBetweenPersons2);
 //-------------------TRANSFORM A ARRAY  WITH MAP-----------------
 //Create new array and modify attribute
-
 // 1 OPTION
 const convertToCms = person => {
   return {
@@ -80,8 +87,32 @@ const convertToCms2 = person => ({
   ...person,
   altura: person.altura * 100
 });
-var PersonsWithTallInCms = Persons.map(convertToCms);
-var PersonsWithTallInCms = Persons.map(convertToCms2);
+
+//3 OPTION
+const addBook = person => {
+  return {
+    ...person,
+    cantidadDeLibros: person.cantidadDeLibros + 1
+  };
+};
+
+var PersonsWithTallInCms1 = Persons.map(convertToCms);
+var PersonsWithTallInCms2 = Persons.map(convertToCms2);
+var PersonsWithTallInCms3 = Persons.map(function(person) {
+  return {
+    ...person,
+    altura: person.altura * 100
+  };
+});
+var PersonWithAddBook = Persons.map(addBook);
+
+console.log("Persons: ", Persons);
+console.log("Convert TO CMS 1: ", PersonsWithTallInCms1);
+console.log("Convert TO CMS 2: ", PersonsWithTallInCms2);
+console.log("Convert TO CMS 3: ", PersonsWithTallInCms3);
+console.log("Add Book", PersonWithAddBook);
+
+console.log("Persons: ", Persons);
 
 //----------------------REDUCE---------------------------------
 // Reduce to an only value
@@ -89,12 +120,61 @@ var PersonsWithTallInCms = Persons.map(convertToCms2);
 
 const countBooks = (total, person) => total + person.cantidadDeLibros;
 const countBooks2 = (total, { cantidadDeLibros }) => total + cantidadDeLibros;
+const countAlturas = (totalAltura, { altura }) => totalAltura + altura;
+
+var totalAlture = Persons.reduce(countAlturas, 0);
+console.log("Total Alturas ", totalAlture);
 
 var TotalOfBooks = Persons.reduce(countBooks, 0);
 var TotalOfBooks2 = Persons.reduce(countBooks2, 0);
+
 var TotalOfBooks3 = Persons.reduce(function(total, person) {
   return total + person.cantidadDeLibros;
 }, 0);
+
 var TotalOfBooks4 = Persons.reduce(function(total, { cantidadDeLibros }) {
   return total + cantidadDeLibros;
 }, 0);
+
+//**************************Exercises******************** */
+
+//Return all persons with most 68 and less 90, add 10 books and sum total books
+const haveManyBooks = ({ cantidadDeLibros }) => {
+  return (cantidadDeLibros > 68) & (cantidadDeLibros < 92);
+};
+
+var personWithManyBooks = Persons.filter(haveManyBooks);
+console.log("----------------------------------------------");
+console.log("Many Books ", personWithManyBooks);
+
+const addBooks = person => {
+  return {
+    ...person,
+    cantidadDeLibros: person.cantidadDeLibros + 10
+  };
+};
+var addBookToPersons = personWithManyBooks.map(addBooks);
+console.log("Addd Books ", addBookToPersons);
+
+var sumBooks = (total, { cantidadDeLibros }) => (total += cantidadDeLibros);
+
+var totalBooks = addBookToPersons.reduce(sumBooks, 0);
+
+console.log("Total Books ", totalBooks);
+
+//Anidade functions
+
+var totalBooks2 = Persons.filter(function(person) {
+  return (person.cantidadDeLibros > 68) & (person.cantidadDeLibros < 92);
+})
+  .map(function(person) {
+    return {
+      ...person,
+      cantidadDeLibros: person.cantidadDeLibros + 10
+    };
+  })
+  .reduce(function(total, { cantidadDeLibros }) {
+    return (total += cantidadDeLibros);
+  }, 0);
+
+console.log("Tootal Books 2 OPCTION", totalBooks2);
